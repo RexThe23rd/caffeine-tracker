e1006408dbdef899c191866137befc222977949b
RexThe23rd <rexthe23rd@gmail.com>
2025-12-04 13:35:31 -0500

    Finished the header & footer in Layout.jsx, fleshed out the Hero.jsx, added the ability to import fontawesome icons in Index.html

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,21 @@
 export default function Hero() {
     return(
         <>
-        
+            <h1>Coffee Tracking for Coffee <abbr title="An enthusiast or devotee">Fiends</abbr>!</h1>
+            <div className="benefits-list">
+                <h3 className="font-bolder">Try <span className="text-gradient">Caffiend</span> and start ...</h3>
+                <p>✅ Tracking every coffee</p>
+                <p>✅ Measuring your blood caffeine levels</p>
+                <p>✅ Costing and quantifying your addiction</p>
+            </div>
+            <div className="card info-card">
+                <div>
+                    <i className="fa-solid fa-circle-info"></i>
+                    <h3>Did you know...</h3>
+                </div>
+                <h5>That caffeine&apos;s half-life is ~5 hrs?</h5>
+                <p>this means that after 5 hours, half the caffeine you consumed is still in your system, keeping you awake longer! So if you drink a cup of coffee with 200mg of caffeine, 5 hours later, you&apos;ll still have about 100mg of caffeine in your system.</p>
+            </div>
         </>
     )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,22 @@ export default function Layout(props) {
     const { children } = props
 
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1 className="text-gradient">CAFFIEND</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
     const footer = (
-        <footer></footer>
+        <footer>
+            <p>This <span className="text-gradient">Caffiend</span> version was made by <i class="fa-brands fa-github"></i><a href="https://github.com/RexThe23rd">RexThe23rd</a> following Smoljames' <a href="https://www.youtube.com/watch?v=iKpkVKubvKk">ReactJS course</a><br></br>
+            <span className="font-bolder">Original made by <a href="https://www.smoljames.com">Smoljames</a></span></p>
+        </footer>
     )
 
     return(
