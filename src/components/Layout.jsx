e1148571143994b4ee58c7225c7f70b4ef3ab383
RexThe23rd <rexthe23rd@gmail.com>
2025-12-08 11:29:22 -0500

    finished the footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,8 +15,8 @@ export default function Layout(props) {
     )
     const footer = (
         <footer>
-            <p>This <span className="text-gradient">Caffiend</span> version was made by <i class="fa-brands fa-github"></i><a href="https://github.com/RexThe23rd">RexThe23rd</a> following Smoljames' <a href="https://www.youtube.com/watch?v=iKpkVKubvKk">ReactJS course</a><br></br>
-            <span className="font-bolder">Original made by <a href="https://www.smoljames.com">Smoljames</a></span></p>
+            <p>This <span className="text-gradient">Caffiend</span> version was made by <i class="fa-brands fa-github"></i><a href="https://github.com/RexThe23rd" target="_blank">RexThe23rd</a> following Smoljames' <a href="https://www.youtube.com/watch?v=iKpkVKubvKk" target="_blank">ReactJS course</a> and using a modified <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br></br>
+            <span className="font-bolder">Original made by <a href="https://www.smoljames.com">Smoljames.</a></span></p>
         </footer>
     )
 
