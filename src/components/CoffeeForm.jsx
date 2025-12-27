ad5fcf0ce8d6a514c06cfd4b1533f0e36e70c02a
RexThe23rd <rexthe23rd@gmail.com>
2025-12-27 15:22:57 -0500

    finished coffee form, basic submit functionalitly (console.log lmao)

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -4,6 +4,13 @@ import { useState } from "react"
 export default function CoffeeForm() {
     const [selectedCoffee, setSelectedCoffee] = useState(null)
     const [showCoffeTypes, setShowCoffeeTypes] = useState(false)
+    const [coffeeCost, setCoffeeCost] = useState(0)
+    const [hour, setHour] = useState(0)
+    const [min, setMin] = useState(0)
+
+    function handleSubmitForm() {
+        console.log(selectedCoffee, coffeeCost, hour, min)
+    }
 
     return(
         <>
@@ -47,12 +54,16 @@ export default function CoffeeForm() {
                 </select>
             )}
             <h4>Add the cost ($)</h4>
-            <input type="number" className="w-full" placeholder="4.50" />
+            <input type="number" className="w-full" value={coffeeCost} onChange={(e) => {
+                setCoffeeCost(e.target.value)
+            }} placeholder="4.50" />
             <h4>Time since consumption</h4>
             <div className="time-entry">
                 <div>
                     <h6>Hours</h6>
-                    <select id="hours-select">
+                    <select onChange={(e) => {
+                        setHour(e.target.value)
+                    }} id="hours-select">
                         {[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23].map((hour, hourIndex) => {
                             return(
                                 <option key={hourIndex} value={hour}>{hour}</option>
@@ -62,7 +73,9 @@ export default function CoffeeForm() {
                 </div>
                 <div>
                     <h6>Mins</h6>
-                    <select id="mins-select">
+                    <select onChange={(e) => {
+                        setMin(e.target.value)
+                    }} id="mins-select">
                         {[0, 5, 10, 15, 30, 45].map((min, minIndex) => {
                             return(
                                 <option key={minIndex} value={min}>{min}</option>
@@ -71,7 +84,7 @@ export default function CoffeeForm() {
                     </select>
                 </div>
             </div>
-            <button>
+            <button onClick={handleSubmitForm}>
                 <p>Add Entry</p>
             </button>
         </>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -15,7 +15,7 @@ export default function Layout(props) {
     )
     const footer = (
         <footer>
-            <p>This <span className="text-gradient">Caffiend</span> version was made by <i class="fa-brands fa-github"></i><a href="https://github.com/RexThe23rd" target="_blank">RexThe23rd</a> following Smoljames' <a href="https://www.youtube.com/watch?v=iKpkVKubvKk" target="_blank">ReactJS course</a><br />using a modified <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br></br>
+            <p>This <span className="text-gradient">Caffiend</span> version was made by <i className="fa-brands fa-github"></i><a href="https://github.com/RexThe23rd" target="_blank">RexThe23rd</a> following Smoljames' <a href="https://www.youtube.com/watch?v=iKpkVKubvKk" target="_blank">ReactJS course</a><br />using a modified <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library.<br></br>
             <span className="font-bolder">Original made by <a href="https://www.smoljames.com">Smoljames.</a></span></p>
         </footer>
     )
