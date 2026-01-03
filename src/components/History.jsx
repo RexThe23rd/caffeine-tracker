34d2b63b53d7f8fb91648e097ef80df50b85881b
RexThe23rd <rexthe23rd@gmail.com>
2026-01-03 13:21:31 -0500

    added history 👍

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,7 +1,32 @@
+import { calculateCurrentCaffeineLevel, coffeeConsumptionHistory, getCaffeineAmount, timeSinceConsumption } from "../utils";
+
 export default function History() {
     return(
         <>
+            <div className="section-header">
+                <i className="fa-solid fa-timeline" />
+                <h2>History</h2>
+            </div>
+            <p><i>Hover for more information</i></p>
+            <div className="coffee-history">
+                {Object.keys(coffeeConsumptionHistory).sort((a, b) => b - a).map((utcTime, coffeeIndex) => {
         
+                    const coffee = coffeeConsumptionHistory[utcTime]
+                    const timeSinceConsume = timeSinceConsumption(utcTime)
+                    const originalAmmount = getCaffeineAmount(coffee.name)
+                    const remainingAmmount = calculateCurrentCaffeineLevel({
+                        [utcTime]: coffee
+                    })     
+                    
+                    const summary = `${coffee.name} | ${timeSinceConsume} | $${coffee.cost} | ${remainingAmmount}mg / ${originalAmmount}mg`
+
+                    return (
+                        <div title={summary} key={coffeeIndex}>
+                            <i className="fa-solid fa-mug-hot"/>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
