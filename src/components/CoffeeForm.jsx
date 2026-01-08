6d3486211c1a9c40e904f238ca39986f827234d3
RexThe23rd <rexthe23rd@gmail.com>
2026-01-08 13:36:19 -0500

    added the stat rendering when theres data, fixed issues with it not rendering properly (friggin TYPOS AAA)

--- src/components/CoffeeForm.jsx
+++ src/components/CoffeeForm.jsx
@@ -2,29 +2,60 @@ import { coffeeOptions } from "../utils"
 import { useState } from "react"
 import Modal from "./Modal"
 import Authentication from "./Authentication"
+import { useAuth } from "../context/AuthContext"
+import { doc, setDoc } from "firebase/firestore"
+import { db } from "../../firebase"
 
 export default function CoffeeForm(props) {
     const { isAuthenticated } = props
-    const [ showModal, setShowModal ] = useState(false)
+    const [showModal, setShowModal] = useState(false)
     const [selectedCoffee, setSelectedCoffee] = useState(null)
-    const [showCoffeTypes, setShowCoffeeTypes] = useState(false)
+    const [showCoffeeTypes, setShowCoffeeTypes] = useState(false)
     const [coffeeCost, setCoffeeCost] = useState(0)
     const [hour, setHour] = useState(0)
     const [min, setMin] = useState(0)
 
-    function handleSubmitForm() {
+    const { globalData, setGlobalData, globalUser } = useAuth()
+
+    async function handleSubmitForm() {
         if (!isAuthenticated) {
             setShowModal(true)
             return
         }
-        console.log(selectedCoffee, coffeeCost, hour, min)
+
+        if (!selectedCoffee) {
+            return
+        }
+
+        const newGlobalData = {
+            ...(globalData || {})
+        }
+
+        const nowTime = Date.now()
+        const timeToSubtract = (hour * 60 * 60 * 1000) + (min * 60 * 1000)
+        const timestamp = nowTime - timeToSubtract
+
+        const newData = {
+            name: selectedCoffee,
+            cost: coffeeCost
+        }
+
+        newGlobalData[timestamp] = newData
+        console.log(timestamp, selectedCoffee, coffeeCost)
+
+        setGlobalData(newGlobalData)
+
+        const userRef = doc(db, 'users', globalUser.uid)
+        const res = await setDoc(userRef, {
+            [timestamp]: newData
+        }, {merge: true})
     }
 
     function handleCloseModal() {
         setShowModal(false)
     }
 
-    return(
+    return (
         <>
             {showModal && (
                 <Modal handleCloseModal={handleCloseModal}>
@@ -32,7 +63,7 @@ export default function CoffeeForm(props) {
                 </Modal>
             )}
             <div className="section-header">
-                <i className="fa-solid fa-pencil"/>
+                <i className="fa-solid fa-pencil" />
                 <h2>Start tracking today</h2>
             </div>
             <h4>Select coffee type</h4>
@@ -51,18 +82,18 @@ export default function CoffeeForm(props) {
                 <button onClick={() => {
                     setShowCoffeeTypes(true)
                     setSelectedCoffee(null)
-                }} className={"button-card " + (showCoffeTypes ? ' coffee-button-selected' : ' ')}>
+                }} className={"button-card " + (showCoffeeTypes ? ' coffee-button-selected' : ' ')}>
                     <h4>Other</h4>
                     <p>n/a</p>
                 </button>
             </div>
-            {showCoffeTypes &&(
+            {showCoffeeTypes && (
                 <select onChange={(e) => {
                     setSelectedCoffee(e.target.value)
                 }} name="coffee-list" id="coffee-list">
                     <option value={null}>Select type</option>
                     {coffeeOptions.map((option, optionIndex) => {
-                        return(
+                        return (
                             <option value={option.name} key={optionIndex}>
                                 {option.name} ({option.caffeine}mg)
                             </option>
@@ -81,8 +112,8 @@ export default function CoffeeForm(props) {
                     <select onChange={(e) => {
                         setHour(e.target.value)
                     }} id="hours-select">
-                        {[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23].map((hour, hourIndex) => {
-                            return(
+                        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map((hour, hourIndex) => {
+                            return (
                                 <option key={hourIndex} value={hour}>{hour}</option>
                             )
                         })}
@@ -94,7 +125,7 @@ export default function CoffeeForm(props) {
                         setMin(e.target.value)
                     }} id="mins-select">
                         {[0, 5, 10, 15, 30, 45].map((min, minIndex) => {
-                            return(
+                            return (
                                 <option key={minIndex} value={min}>{min}</option>
                             )
                         })}
